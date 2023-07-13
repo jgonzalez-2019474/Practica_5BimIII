d41bcaa2b5c78114edff3a1a82d22d273f113e72
jgonzalez-2019474 <129011598+jgonzalez-2019474@users.noreply.github.com>
2023-07-13 08:56:03 -0600

    Update Practica_Sensor_MQ2.ino

--- Practica_Sensor_MQ2.ino
+++ Practica_Sensor_MQ2.ino
@@ -1,3 +1,15 @@
+/*
+ * Fundacion Kinal
+ * Centro educativo tecnico laboral Kinal
+ * Electronica
+ * Grado: Quinto
+ * Sección: A
+ * Curso: Taller de electronica digital y reparación de computadoras I
+ * Nombre: Javier Andre Gonzalez Barrera
+ * Carnet: 2019474
+ * Proyecto: Practica #2
+*/
+
 #include <LedControl.h>
 #include <Ticker.h>
 
